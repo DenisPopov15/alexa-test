a46c4b78c92761dccaf95f4e796e0dcaa3686ee4
DenisPopov15 <denis@slatestudio.com>
2017-05-14 19:52:27 +0300

    Try Just skill-sdk

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 'use strict'
-const alexaHandlers = require('./alexa.js')
+// const alexaHandlers = require('./alexa.js')
 
-require('dotenv').config()
-const app = require('express')()
+// require('dotenv').config()
+// const app = require('express')()
 
-app.post('/', function (req, res) {
-  res.send(alexaHandlers.handler)
-})
+// app.post('/', function (req, res) {
+//   res.send(alexaHandlers.handler)
+// })
 
-app.listen(process.env.PORT, function () {
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-})
+// app.listen(process.env.PORT, function () {
+//   console.log(`Example app listening on port ${process.env.PORT}!`)
+// })
+
+const Alexa = require('alexa-sdk')
+
+exports.handler = function(event, context, callback) {
+  const alexa = Alexa.handler(event, context)
+  alexa.registerHandlers(handlers)
+  alexa.execute()
+}
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('SayHello')
+    },
+    'DenisHelloWorldIntent': function () {
+        this.emit('SayHello')
+    },
+    'SayHello': function () {
+        this.emit(':tell', 'Hello World!')
+    }
+}
